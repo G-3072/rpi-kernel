045a93eb35d890b0f0f502b2f853e5c49850683b
Gaudenz Schweizer <gaudenz.schweizer@cedes.com>
2025-07-18 15:03:33 +0200

    start mini UART

--- include/kernel/peripherals.h
+++ include/kernel/peripherals.h
@@ -9,33 +9,38 @@ enum Peripherals
     PERIPHERAL_BASE = 0x20000000U,
     // peripheral offsets
     GPIO_OFFSET = 0x200000U,
-    UART0_OFFSET = 0x201000U,
+    UART_OFFSET = 0x201000U,
     // periphaeral base addresses
     GPIO_BASE = (PERIPHERAL_BASE + GPIO_OFFSET),
-    UART0_BASE = (PERIPHERAL_BASE + UART0_OFFSET),
+    UART_BASE = (PERIPHERAL_BASE + UART_OFFSET),
 
 };
 
 enum UART
 {
-    UART0_DR        = (UART0_BASE + 0x00),
-    UART0_RSRECR    = (UART0_BASE + 0x04),
-    UART0_FR        = (UART0_BASE + 0x18),
-    UART0_ILRP      = (UART0_BASE + 0x20),
-    UART0_IBRD      = (UART0_BASE + 0x24),
-    UART0_FBRD      = (UART0_BASE + 0x28),
-    UART0_LCRH      = (UART0_BASE + 0x2c),
-    UART0_CR        = (UART0_BASE + 0x30),
-    UART0_IFLS      = (UART0_BASE + 0x34),
-    UART0_IMSC      = (UART0_BASE + 0x38),
-    UART0_RIS       = (UART0_BASE + 0x3c),
-    UART0_MIS       = (UART0_BASE + 0x40),
-    UART0_ICR       = (UART0_BASE + 0x44),
-    UART0_DMACR     = (UART0_BASE + 0x48),
-    UART0_ITCR      = (UART0_BASE + 0x80),
-    UART0_ITIP      = (UART0_BASE + 0x84),
-    UART0_ITOP      = (UART0_BASE + 0x88),
-    UART0_TDR       = (UART0_BASE + 0x8c),
+    UART_DR        = (UART_BASE + 0x00),
+    UART_RSRECR    = (UART_BASE + 0x04),
+    UART_FR        = (UART_BASE + 0x18),
+    UART_ILRP      = (UART_BASE + 0x20),
+    UART_IBRD      = (UART_BASE + 0x24),
+    UART_FBRD      = (UART_BASE + 0x28),
+    UART_LCRH      = (UART_BASE + 0x2c),
+    UART_CR        = (UART_BASE + 0x30),
+    UART_IFLS      = (UART_BASE + 0x34),
+    UART_IMSC      = (UART_BASE + 0x38),
+    UART_RIS       = (UART_BASE + 0x3c),
+    UART_MIS       = (UART_BASE + 0x40),
+    UART_ICR       = (UART_BASE + 0x44),
+    UART_DMACR     = (UART_BASE + 0x48),
+    UART_ITCR      = (UART_BASE + 0x80),
+    UART_ITIP      = (UART_BASE + 0x84),
+    UART_ITOP      = (UART_BASE + 0x88),
+    UART_TDR       = (UART_BASE + 0x8c),
+};
+
+enum MUART
+{
+    MUART_ = 0,
 };
 
 enum GPIO
--- src/kernel/uart.c
+++ src/kernel/uart.c
@@ -13,7 +13,7 @@
  */
 void uart_init(void){
     
-    mmio_write(UART0_CR, 0x0);  // disables UART so it can be configured
+    mmio_write(UART_CR, 0x0);  // disables UART so it can be configured
 
     mmio_write(GPPUD, 0x0); // disable GPIO Pull-Ups/Downs
     delay(150);
@@ -23,25 +23,25 @@ void uart_init(void){
 
     mmio_write(GPPUDCLK0, 0x0);
 
-    mmio_write(UART0_ICR, 0x7FF);   // clears all intterupts
+    mmio_write(UART_ICR, 0x7FF);   // clears all intterupts
 
     uart_setBaudrate(115200);
 
-    mmio_write(UART0_LCRH, (1<<4)|(1<<5)|(1<<6));    // set to 8bit transmit and enable FIFO transmit buffer
+    mmio_write(UART_LCRH, (1<<4)|(1<<5)|(1<<6));    // set to 8bit transmit and enable FIFO transmit buffer
 
-    mmio_write(UART0_IMSC, (1<<1)|(1<<4)|(1<<5)|(1<<6)|(1<<7)|(1<<8)|(1<<9)|(1<<10));   // enabele interruppts
+    mmio_write(UART_IMSC, (1<<1)|(1<<4)|(1<<5)|(1<<6)|(1<<7)|(1<<8)|(1<<9)|(1<<10));   // enabele interruppts
 
-    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));   // turn UART back on
+    mmio_write(UART_CR, (1 << 0) | (1 << 8) | (1 << 9));   // turn UART back on
 }
 
 void uart_putc(unsigned char c){
-    while(mmio_read(UART0_FR) & (1<<5));
-    mmio_write(UART0_DR, c);
+    while(mmio_read(UART_FR) & (1<<5));
+    mmio_write(UART_DR, c);
 }
 
 unsigned char uart_getc(void){
-    while(mmio_read(UART0_FR) & (1<<4));
-    return mmio_read(UART0_DR);
+    while(mmio_read(UART_FR) & (1<<4));
+    return mmio_read(UART_DR);
 }
 
 void uart_puts(const char* str){
@@ -52,11 +52,11 @@ void uart_puts(const char* str){
 
 void uart_setBaudrate(uint32_t baudrate){
     uint32_t ibrd_val = (uint32_t)(FUARTCLK/(16*baudrate));
-    mmio_write(UART0_IBRD, ibrd_val);
+    mmio_write(UART_IBRD, ibrd_val);
 
     uint32_t fbrd_val;
     float divisor = (FUARTCLK/(16*(float)baudrate));
     divisor -= ibrd_val;
     fbrd_val = (uint32_t)(divisor * 64 + 0.5f);
-    mmio_write(UART0_FBRD, fbrd_val);
+    mmio_write(UART_FBRD, fbrd_val);
 }
